f7c82e9f3efc4aad236e34391992ed122546fa94
Jack-Cui <826885122@qq.com>
2025-08-23 20:09:30 +0800

    提交唤醒app代码

--- src/components/DownloadAppModal.tsx
+++ src/components/DownloadAppModal.tsx
@@ -80,10 +80,9 @@ const DownloadAppModal: React.FC<DownloadAppModalProps> = ({
       signature = generateSignature(nonceStr, strtimestamp, url, jsapi_ticket);  
       console.log('signature:', signature);
 
-
+      alert('isAndroid:'+isAndroid+';isWeChat:'+isWeChat);
       if (isAndroid){
         if(isWeChat){
-
           wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印
             appId: appId, // 必填，公众号的唯一标识
