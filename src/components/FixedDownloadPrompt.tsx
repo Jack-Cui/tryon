d8b47b45f0d46ad887201364009f2b6e02e07bd0
Jack-Cui <826885122@qq.com>
2025-09-19 08:32:47 +0800

    修改app唤起和下载功能

--- src/components/FixedDownloadPrompt.tsx
+++ src/components/FixedDownloadPrompt.tsx
@@ -169,15 +169,11 @@ const FixedDownloadPrompt: React.FC = () => {
                   openTagList: [
                     'wx-open-launch-app'
                   ] // 可选，需要使用的开放标签列表，例如['wx-open-launch-app']
-                });
-                
-
+                });                
           } catch (error: any) {
             console.error('处理失败:', error);
           }
-        })();
-      
-        
+        })();              
       } catch (error) {
         console.error('唤起APP失败:', error);
       }
